32830be0bad058e43fa504a3092abd6adb8bd5c4
Paul Petrut <me@Pauls-MacBook-Pro.local>
2025-11-17 17:11:10 +0200

    Remove unused phoenix-colocated import

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,6 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
-import { hooks as colocatedHooks } from "phoenix-colocated/sahajyog"
 import topbar from "../vendor/topbar"
 
 const Hooks = {}
@@ -73,7 +72,7 @@ const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: { ...colocatedHooks, ...Hooks },
+  hooks: Hooks,
 })
 
 // Show progress bar on live navigation and form submits
