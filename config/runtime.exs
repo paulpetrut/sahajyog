afee2e763253001b682c57e878bced7901b4672f
Paul Petrut <me@Pauls-MacBook-Pro.local>
2025-11-17 17:59:51 +0200

    Fix WebSocket origin check for production deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -68,6 +68,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    check_origin: ["https://#{host}", "//#{host}"],
     secret_key_base: secret_key_base
 
   # ## SSL Support
