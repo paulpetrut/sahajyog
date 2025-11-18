c60312149ab5477ca27cda480456fbcad6e64e9d
Paul Petrut <me@Pauls-MacBook-Pro.local>
2025-11-18 19:27:01 +0200

    Add translations for authentication messages in all languages

--- lib/sahajyog_web/controllers/user_session_controller.ex
+++ lib/sahajyog_web/controllers/user_session_controller.ex
@@ -5,11 +5,11 @@ defmodule SahajyogWeb.UserSessionController do
   alias SahajyogWeb.UserAuth
 
   def create(conn, %{"_action" => "confirmed"} = params) do
-    create(conn, params, "User confirmed successfully.")
+    create(conn, params, gettext("User confirmed successfully."))
   end
 
   def create(conn, params) do
-    create(conn, params, "Welcome back!")
+    create(conn, params, gettext("Welcome back!"))
   end
 
   # magic link login
@@ -24,12 +24,15 @@ defmodule SahajyogWeb.UserSessionController do
 
       {:error, :password_required} ->
         conn
-        |> put_flash(:info, "Your account has been confirmed! Please log in with your password.")
+        |> put_flash(
+          :info,
+          gettext("Your account has been confirmed! Please log in with your password.")
+        )
         |> redirect(to: ~p"/users/log-in")
 
       _ ->
         conn
-        |> put_flash(:error, "The link is invalid or it has expired.")
+        |> put_flash(:error, gettext("The link is invalid or it has expired."))
         |> redirect(to: ~p"/users/log-in")
     end
   end
@@ -45,7 +48,7 @@ defmodule SahajyogWeb.UserSessionController do
     else
       # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
       conn
-      |> put_flash(:error, "Invalid email or password")
+      |> put_flash(:error, gettext("Invalid email or password"))
       |> put_flash(:email, String.slice(email, 0, 160))
       |> redirect(to: ~p"/users/log-in")
     end
@@ -61,12 +64,12 @@ defmodule SahajyogWeb.UserSessionController do
 
     conn
     |> put_session(:user_return_to, ~p"/users/settings")
-    |> create(params, "Password updated successfully!")
+    |> create(params, gettext("Password updated successfully!"))
   end
 
   def delete(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
+    |> put_flash(:info, gettext("Logged out successfully."))
     |> UserAuth.log_out_user()
   end
 end
